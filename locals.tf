75240d9db1619d42c70af60a67a13a7a0b6730aa
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-07-01 15:47:27 +0530

    tfupgrade 0.12

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
-  l3_repo         = "${data.aws_region.current.name == "us-east-1" ? local.l3_repo_base : local.l3_repo_region}"
-  l3_repo_base    = "lambdalambdalambda-repo"
-  l3_repo_region  = "${local.l3_repo_base}-${data.aws_region.current.name}"
+  l3_repo        = data.aws_region.current.name == "us-east-1" ? local.l3_repo_base : local.l3_repo_region
+  l3_repo_base   = "lambdalambdalambda-repo"
+  l3_repo_region = "${local.l3_repo_base}-${data.aws_region.current.name}"
 }
+
